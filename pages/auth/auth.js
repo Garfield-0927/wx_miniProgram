761c89202f87983ae5e3aaaf2121a2c79aa29d20
Garfield_zhang <929780650@qq.com>
2020-09-28 23:37:53 +0800

    buzhidao

--- pages/auth/auth.js
+++ pages/auth/auth.js
@@ -8,59 +8,28 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getuserinfoHandler(e){
+    const {encData, rawData, iv, signature} = e.detail;
+    wx.login({
+      timeout: 10000,
+      success:(res) => {
+        const code = res.code;
+        console.log(code);
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' +'wxf9072fc16c98ba8a'+ '&secret=' + '1822332bc1eefabe68a6c5f43995be7c' + '&js_code=' + code + '&grant_type=authorization_code',
+          data: {},
+          header: {
+            'content-type': 'json'
+          },
+          success: function (res) {
+            console.log(res);
+            const openid = res.data.openid //返回openid
+            console.log('openid为' + openid);
+          }
+        })
+        
+      }
+    })
   }
+
 })
